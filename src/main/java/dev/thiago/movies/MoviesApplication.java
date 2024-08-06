24d347d7d5a9e64a6e4240a8d62c0d1830af015e
thiagonzalezz <thiagogleyes@gmail.com>
2024-08-05 22:46:45 -0300

    backend finished

--- src/main/java/dev/thiago/movies/MoviesApplication.java
+++ src/main/java/dev/thiago/movies/MoviesApplication.java
@@ -3,11 +3,14 @@ package dev.thiago.movies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class MoviesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MoviesApplication.class, args);
 	}
+
+
 
 }
